0ec24bf900015dc9607f8d45e66b25dde9f3387f
Ricky Thakar <rickythakar@gmail.com>
2022-10-30 12:26:25 -0400

    finished application

--- script.js
+++ script.js
@@ -1,46 +1,35 @@
-// assigning variables
-// let eightBallIndex = 0;
-document.getElementById("shakeButton").addEventListener("click", magicEightBall);
+//Declare Variables
+let button = document.querySelector("button");
+let answer = document.querySelector("#answer");
 
-function magicEightBall(){
-    alert("clicked")
-}
+// // Creating an array
+let responseArray = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes, definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+]
 
-let answer = document.querySelector("#answer")
-
-// Add a click event to the button
-// button.onclick =
-//     () => {
-//         alert("message: clicked");
-//     }
-
-// // creating an array
-// let responseArray = [
-//     "It is certain.", 
-//     "It is decidedly so.", 
-//     "Without a doubt.", 
-//     "Yes, definitely.", 
-//     "You may rely on it.", 
-//     "As I see it, yes.", 
-//     "Most likely.", 
-//     "Outlook good.", 
-//     "Yes.", 
-//     "Signs point to yes.", 
-//     "Reply hazy, try again.", 
-//     "Ask again later.", 
-//     "Better not tell you now.", 
-//     "Cannot predict now.", 
-//     "Concentrate and ask again.", 
-//     "Don't count on it.", 
-//     "My reply is no.", 
-//     "My sources say no.", 
-//     "Outlook not so good.", 
-//     "Very doubtful."
-// ]
-
-// // creating a function - loops through array & get document.getElementByID("input-id") & set up a HTML/listener event
-// // Math.floor() to randomize array
-// let randomizeAnswer = () => {
-//     eightBallIndex = Math.floor(Math.random()) * responseArray.lenght);
-//     console.log(eightBallIndex);
-// 
+// Hooking into Shake button and adding an event listener 
+button.addEventListener("click", function () {
+    // Generate random number and hook into array to grab a random index
+    let answerText = responseArray[Math.floor(Math.random() * responseArray.length)];
+    // Replacing the text content inside the answer text
+    answer.textContent = answerText
+});
